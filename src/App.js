e1b56ac015bd85daaa10bfc7c59b3f6c32667eb6
ddalkigum <sol35352000@gmail.com>
2020-09-19 15:52:19 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>hi</h1>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    ChampName: "Yasuo",
+    Attack: 58,
+  };
 
+  add = () => {
+    this.setState((current) => ({ Attack: current.Attack + 1 }));
+  };
+  minus = () => {
+    this.setState({ Attack: this.state.Attack - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Damage: {this.state.Attack}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
 export default App;
